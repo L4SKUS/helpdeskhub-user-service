06370c18f1a6ef0f02cc210ee1dcfab2f71767ac
blaskus <SU?uaJ@>
2025-03-26 15:09:39 +0100

    initial commit

---
+++ src/main/java/com/helpdeskhub/users/entity/User.java
@@ -0,0 +1,4 @@
+package com.helpdeskhub.users.entity;
+
+public class User {
+}
---
+++ src/test/java/com/helpdeskhub/users/UsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helpdeskhub.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
