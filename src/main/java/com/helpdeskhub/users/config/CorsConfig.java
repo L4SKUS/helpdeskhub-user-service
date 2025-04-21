81cecad57ac52fe0d3573aedb11de3f9137f06b9
blaskus <SU?uaJ@>
2025-04-21 22:46:59 +0200

    add cors

---
+++ src/main/java/com/helpdeskhub/users/config/CorsConfig.java
@@ -0,0 +1,17 @@
+package com.helpdeskhub.users.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173") // Your React port
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
